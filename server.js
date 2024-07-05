e95b4d186b00ecc4be4d089fc8cb04d1afbeb1e6
roughnut <jeremy@writever.com.au>
2024-07-05 14:27:43 +1000

    add server.js and connection.js files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+db.once("connection", () => {
+    app.listen(PORT, () => {
+        console.log(`Connected to server on http://localhost:${PORT} `);
+    });
+});
